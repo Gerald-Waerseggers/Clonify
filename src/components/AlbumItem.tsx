a1f32eb8efc22c08f850ad32b31ec750c8913aee
Gerald-Waerseggers <r0853509@ucll.be>
2024-08-25 11:04:54 +0200

    Display UI

---
+++ src/components/AlbumItem.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+interface AlbumItemProps {
+    image: string;
+    name: string;
+    desc: string;
+    id: number;
+  }
+  const AlbumItem: React.FC<AlbumItemProps> = ({ image, name, desc }) => {
+      
+  
+  return (
+    <div className='min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
+        <img className='rounded' src={image} alt="" />
+        <p className='font-bold mt-2 mb-1'>{name}</p>
+        <p className='text-slate-200 text-sm'>{desc}</p>
+    </div>
+  )
+}
+
+export default AlbumItem
